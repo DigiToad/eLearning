3f7bb65146577070b6ec3c863e26011527f34d52
Bhavana R <bhavanabanu2028@gmail.com>
2026-06-11 17:22:34 +0530

    logout

---
+++ src/routes/logout/+page.server.js
@@ -0,0 +1,31 @@
+import { auth } from '$lib/server/lucia.js';
+import { redirect } from '@sveltejs/kit';
+
+export async function load({ locals, cookies }) {
+  try {
+    if (locals.authedUser) {
+      const sessionIdToInvalidate = locals.session?.sessionId;
+      
+      if (sessionIdToInvalidate) {
+        await auth.invalidateSession(sessionIdToInvalidate);
+      }
+    }
+    
+    // Clear locals
+    locals.authedUser = undefined;
+    locals.session = undefined;
+    
+    // Delete ALL cookies to ensure cleanup
+    const allCookies = cookies.getAll();
+    for (const cookie of allCookies) {
+      cookies.delete(cookie.name, { path: '/' });
+    }
+    
+    // console.log('All cookies deleted, redirecting to login page');
+  } catch (err) {
+    // console.error('Error during logout:', err);
+  }
+  
+  // Throw redirect AFTER all operations complete
+  throw redirect(302, '/login?reload=true');
+}
